ba9342c63ab71430ea8ce6cc3ebd27e9e1bf22fe
Jan Mouwes <jan.mouwes@gmail.com>
2024-06-06 14:27:38 +0200

    ran formatter on sprite.cpp and sprite.h

--- template/sprite.h
+++ template/sprite.h
@@ -2,31 +2,33 @@
 
 namespace Tmpl8
 {
+    class Sprite
+    {
+    public:
+        Sprite(const char* fileName);
+        Sprite(const char* fileName, int2 topLeft, int2 bottomRight, int size, int frames);
+        Sprite(const char* fileName, int frames);
+        void ScaleAlpha(uint scale);
+        uint *pixels, **scaledPixels = 0;
+        int frameCount, frameSize;
+        int **left, **right;
+    };
 
-class Sprite
-{
-public:
-	Sprite( const char* fileName );
-	Sprite( const char* fileName, int2 topLeft, int2 bottomRight, int size, int frames );
-	Sprite( const char* fileName, int frames );
-	void ScaleAlpha( uint scale );
-	uint* pixels, **scaledPixels = 0;
-	int frameCount, frameSize;
-	int** left, **right;
-};
+    class SpriteInstance
+    {
+    public:
+        SpriteInstance() = default;
 
-class SpriteInstance
-{
-public:
-	SpriteInstance() = default;
-	SpriteInstance( Sprite* s ) : sprite( s ) {}
-	void Draw( Surface* target, float2 pos, int frame );
-	void DrawAdditive( Surface* target, float2 pos, int frame );
-	void Remove();
-	Sprite* sprite = 0;
-	uint* backup = 0;
-	int2 lastPos;
-	Surface* lastTarget = 0;
-};
+        SpriteInstance(Sprite* s) : sprite(s)
+        {
+        }
 
+        void Draw(Surface* target, float2 pos, int frame);
+        void DrawAdditive(Surface* target, float2 pos, int frame);
+        void Remove();
+        Sprite* sprite = 0;
+        uint* backup = 0;
+        int2 lastPos;
+        Surface* lastTarget = 0;
+    };
 } // namespace Tmpl8
