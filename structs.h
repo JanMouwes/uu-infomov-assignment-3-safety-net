292ded8f082437c8e115f9bb5861f2cb1e37c069
toffernator <74768013+toffernator@users.noreply.github.comchris>
2024-06-11 06:09:05 -0700

    chore: Remove Drawable
    VisualComponent / AnimComponent will replace it.

--- structs.h
+++ structs.h
@@ -17,30 +17,4 @@ namespace Tmpl8
         SpriteInstance sprite;
         int frame;
     };
-
-
-    struct Drawable
-    {
-        float2 pos;
-        float2 lastPos;
-
-        /**
-         * The base image
-         */
-        Sprite* sprite;
-
-        /**
-         * TODO unknown
-         */
-        uint* backup = 0;
-
-        Surface* lastTarget = 0;
-    };
-
-    inline Drawable init_drawable(const float2 pos, Sprite* sprite)
-    {
-        return {pos, 0, sprite, nullptr, nullptr};
-    }
-
-    void draw_drawable(Surface* target, Drawable drawable, int frame);
 }
